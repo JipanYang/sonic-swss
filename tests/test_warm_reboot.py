b28e908a24a530e11271017e582eaac57064af46
Jipan Yang <jipan.yang@alibaba-inc.com>
2018-10-31 01:42:00 -0700

    Try dvs.SubscribeAsicDbObject() test function
    Signed-off-by: Jipan Yang <jipan.yang@alibaba-inc.com>

--- tests/test_warm_reboot.py
+++ tests/test_warm_reboot.py
@@ -316,10 +316,15 @@ def test_swss_warm_restore(dvs):
     state_db = swsscommon.DBConnector(swsscommon.STATE_DB, dvs.redis_sock, 0)
     restart_count = swss_get_RestoreCount(dvs, state_db)
     dvs.stop_swss()
-    time.sleep(5)
+    time.sleep(10)
     dvs.runcmd("mv /var/log/swss/sairedis.rec /var/log/swss/sairedis.rec.b")
+    pubsub = dvs.SubscribeAsicDbObject("SAI_OBJECT_TYPE")
     dvs.start_swss()
     time.sleep(10)
+
+    (nadd, ndel) = dvs.CountSubscribedObjects(pubsub)
+    assert nadd == 0
+    assert ndel == 0
 
     checkCleanSaiRedisCSR(dvs)
 
